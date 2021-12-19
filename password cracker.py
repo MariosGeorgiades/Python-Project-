e002df0a7a4ab94365ba977749c4b84cf99661b6
MariosTech <96368918+MariosTech@users.noreply.github.com>
2021-12-19 09:52:49 +0200

    Add files via upload

---
+++ password cracker.py
@@ -0,0 +1,16 @@
+from random import *
+while True:
+    user_pass = input("enter your pasword")
+    password = ['a','b','c','d','e','f','g','h','i',
+    'j','k','l','m','n','o','p','q','r','s','t','u',
+    'v','w','x','y','z', ' ']
+
+    guess = ""
+
+    while (guess != user_pass):
+        guess = ""
+        for letter in range(len(user_pass)):
+            guess_letter = password[randint(0, 26)]
+            guess = str(guess_letter) + str(guess)
+        print(guess)
+    print("your pssword is",guess)
