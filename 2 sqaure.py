1aafd1fe4d30aa649babb48dd8fa0cc988f79a14
Marios Georgiades <96368918+MariosTech@users.noreply.github.com>
2021-12-20 08:10:38 +0200

    Add files via upload

---
+++ 2 sqaure.py
@@ -0,0 +1,37 @@
+import turtle
+
+bob = turtle.Turtle()
+
+# bob.forward(100)
+# bob.left(90)
+# bob.forward(100)
+# bob.right(90)
+# bob.forward(100)
+
+bob.color("blue", "cyan")
+
+
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+
+bob.penup()
+bob.forward(150)
+bob.pendown()
+
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+
+
+
+
+turtle.done()
---
+++ Star.py
@@ -0,0 +1,7 @@
+import turtle
+pen = turtle.Turtle()
+n = 200
+for i in range(n):
+    pen.forward(i * 10)
+    pen.right(144)
+turtle.done()
